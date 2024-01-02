5f1b16dab75ac6e53bf22b1b40836c13ab6704cd
Zhiyao <15222848787@163.com>
2024-01-02 11:32:04 -0800

    Add prob

---
+++ Array/two_sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numDic = dict()
+        n = len(nums)
+        for i in range(n):
+            diff = target- nums[i]
+            if diff in numDic:
+                return [numDic[diff], i]
+            numDic[nums[i]] = i
+        return []
